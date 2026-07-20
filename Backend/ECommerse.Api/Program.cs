bd0cb76d09eb69e79d6d39cacda9ed146dcebe79
Ashor Khizeran <khizeran.ashor@gmail.com>
2026-07-20 18:51:16 -0400

    Frontnd trigger an async API call with help of http client and minimal API endpoint is implemented. An Results.ok(product) is returned for now with querying the database since there is no database setup configured yet

--- Backend/ECommerse.Api/Program.cs
+++ Backend/ECommerse.Api/Program.cs
@@ -1,8 +1,33 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(options =>
+    {
+        options
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseCors();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapPost("api/products", async (HttpRequest searchRequest) =>
+{
+    var searchProduct = await JsonSerializer.DeserializeAsync<Product>(searchRequest.Body);
+
+    return Results.Ok(searchProduct);
+});
 
 app.Run();
+
+record Product(Guid Id, string Name);
